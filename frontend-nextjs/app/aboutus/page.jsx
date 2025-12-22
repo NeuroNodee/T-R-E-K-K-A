d025e431e122f1d88d4467a4a82244840efe256c
Kunjan Bhatt <bhattakunjan10@gmail.com>
2025-12-22 21:59:00 +0545

    About US Animation

--- frontend-nextjs/app/aboutus/page.jsx
+++ frontend-nextjs/app/aboutus/page.jsx
@@ -1,5 +1,7 @@
 "use client";
 import Navbar1 from "@/components/Navbar1";
+import { AnimatedTooltip } from "@/components/ui/animated-tooltip";
+
 import { useState } from "react";
 const AboutUs = () => {
     const [sec1, setSec1] = useState(0);
@@ -51,22 +53,22 @@ const AboutUs = () => {
         "Send a connect request to plan trips"
     ];
     const img1 = [
-        "/img11.svg",
-        "/img12.svg",
-        "/img13.svg",
-        "/img14.svg",
-        "/img15.svg"
+        "./img11.svg",
+        "./img12.svg",
+        "./img13.svg",
+        "./img14.svg",
+        "./img15.svg"
     ]
     const img2 = [
-        "/img21.svg",
-        "/img22.svg",
-        "/img23.svg",
-        "/img24.svg"
+        "./img21.svg",
+        "./img22.svg",
+        "./img23.svg",
+        "./img24.svg"
     ]
     const img3 = [
-        "/img31.svg",
-        "/img32.svg",
-        "/img33.svg"
+        "./img31.svg",
+        "./img32.svg",
+        "./img33.svg"
     ]
     const [animationText1, setAnimationText1] = useState(text1[0]);
     const [animationText2, setAnimationText2] = useState(text2[0]);
@@ -74,6 +76,32 @@ const AboutUs = () => {
     const [animationImg1, setAnimationImg1] = useState(img1[0]);
     const [animationImg2, setAnimationImg2] = useState(img2[0]);
     const [animationImg3, setAnimationImg3] = useState(img3[0]);
+    const people = [
+    {
+        id: 1,
+        name: "Kunjan Bhatt",
+        designation: "Lead Developer & Founder",
+        image: "./kunjan.jpeg",
+    },
+    {
+        id: 2,
+        name: "Ashok Koirala",
+        designation: "Co-Founder & AI Engineer",
+        image: "./ashok.jpeg",
+    },
+    {
+        id: 3,
+        name: "Bishnu Upadhyay",
+        designation: "AI Agent Dev",
+        image: "./bishnu.jpeg",
+    },
+    {
+        id: 4,
+        name: "Sonu Jaiswal",
+        designation: "Backend Developer",
+        image:"./sonu.png",
+    },
+    ];
     return (
         <div className="aboutus-content">
             <Navbar1 />
@@ -82,6 +110,35 @@ const AboutUs = () => {
                 <div className="slogan">
                     <p>Welcome to <span>TREKYA</span> – Your Personalized Travel Companion</p>
                     <h2>---Luxury Reimagined---</h2>
+                </div>
+                <div className="hover-about">
+                    <section>
+                        {/* <p>Explore the world with us</p> */}
+                        <ul className="code">
+                            <div className="pretext">
+                                {/* <p>Hover To Reveal</p> */}
+                                <p>Explore the world with us</p>
+                            </div>
+                            <li tabIndex="0" className="digit">
+                                <span>50+<br />Destinations</span>
+                            </li>
+                            <li tabIndex="0" className="digit">
+                                <span>100%<br />Personalized</span>
+                            </li>
+                            <li tabIndex="0" className="digit">
+                                <span>AI-Driven<br />Planning</span>
+                            </li>
+                            <li tabIndex="0" className="digit">
+                                <span>Local<br />Expertise</span>
+                            </li>
+                            <li className="digit">
+                                <span>Sacred<br />Landscapes</span>
+                            </li>
+                            <li className="digit">
+                                <span>Untouched<br />Paths</span>
+                            </li>
+                        </ul>
+                    </section>
                 </div>
                 <div className="work-content">
                     <div className="work">
@@ -135,6 +192,13 @@ const AboutUs = () => {
                         </div>
                     </div>
                 </div>
+                <div className="people">
+                    <h2>Meet Our Team</h2>
+                    <div className="flex items-center justify-center mb-5 w-full">
+                        <AnimatedTooltip items={people} />
+                    </div>
+                </div>
+                
             </div>
         </div>
     );
